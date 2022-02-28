00e0d70f5b8663edfebec645953bc5f9858421cc
Prajin Maharjan <prajin@rarait.com>
2022-02-28 15:05:14 +0545

    js update

--- assets/js/alert.js
+++ assets/js/alert.js
@@ -1,35 +1,44 @@
-window.alert("Hello & welcome to shopify");
+window.addEventListener('DOMContentLoaded', function() {
+    var popContainer = document.getElementById('popupAdContainer');
+    var hiddenButton = document.createElement('button');
+    hiddenButton.id= 'hidden-button';
+    hiddenButton.hidden=true;
+    hiddenButton.setAttribute('data-open','pop-modal');
+    popContainer.appendChild(hiddenButton);
 
-window.addEventListener("load", function() {
-    var parentDiv = document.getElementById("popupAdContainer");
-    parentDiv.className= "popup-modal";
+    var parentDiv = document.createElement('div');
+    parentDiv.className= 'popup-modal';
+    parentDiv.setAttribute('data-animation','slideInOutLeft');
 
-    var div1 = document.createElement("div");
-    div1.className= "popup-modal-dialog";
+    var div1 = document.createElement('div');
+    div1.className= 'popup-modal-dialog';
 
-    var header = document.createElement("header");
-    header.className= "popup-modal-header";
-    header.innerHTML= "This is just header";
+    var header = document.createElement('header');
+    header.className= 'popup-modal-header';
+    header.innerHTML= 'This is just header';
     
-    var closeButton = document.createElement("button");
-    closeButton.className= "pop-modal-close";
-    closeButton.ariaLabel = "close modal";
+    var closeButton = document.createElement('button');
+    closeButton.className= 'pop-modal-close';
+    closeButton.setAttribute('aria-label', 'close modal');
     header.appendChild(closeButton);
 
-    var section = document.createElement("section");
-    section.className = "pop-modal-content";
+    var section = document.createElement('section');
+    section.className = 'pop-modal-content';
 
-    var sectionContent ="<p><strong>Press ✕, ESC, or click outside of the modal to close it</strong></p>";
-    var sectionMainContent ="<p>John Doe is missing!</p>"
+    var sectionContent ='<p><strong>Press ✕, ESC, or click outside of the modal to close it</strong></p>';
+    var sectionMainContent ='<p>John Doe is missing!</p>'
     section.innerHTML =sectionContent;
     section.innerHTML +=sectionMainContent;
     
-    var footer= document.createElement("footer");
-    footer.className= "pop-modal-footer";
-    footer.innerHTML= "Just a normal footer. Nothing to see here!";
+    var footer= document.createElement('footer');
+    footer.className= 'pop-modal-footer';
+    footer.innerHTML= 'Just a normal footer. Nothing to see here!';
 
     div1.appendChild(header);
     div1.appendChild(section);
     div1.appendChild(footer)
     parentDiv.appendChild(div1);
+    popContainer.appendChild(parentDiv);
+
+    this.document.getElementById('hidden-button').click();
 })
